7e442c5849a6e127c0a4ce43dbda377d72ff9d37
faiz alwan rickwanto <nrickwanto@gmail.com>
2022-03-09 21:43:24 +0700

    connect postgre, list done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,64 @@
-var express = require('express');
+var express = require("express");
+const { rows } = require("pg/lib/defaults");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+module.exports = function (db) {
+  /* GET home page. */
+  router.get("/", function (req, res) {
+    const url = req.url == "/" ? "/?page=1" : req.url;
 
-module.exports = router;
+    let params = [];
+
+    if (req.query.string) {
+      params.push(`stringdata ilike '%${req.query.string}%'`);
+    }
+    if (req.query.integer) {
+      params.push(`integerdata = ${req.query.integer}`);
+    }
+    if (req.query.float) {
+      params.push(`floatdata like '${req.query.float}' `);
+    }
+    if (req.query.startDate && req.query.endDate) {
+      params.push(
+        `datedata BETWEEN DATE('${req.query.startDate}') AND DATE('${req.query.endDate}') `
+      );
+    }
+    if (req.query.boolean) {
+      params.push(`booleandata = ${req.query.boolean}`);
+    }
+
+    const page = req.query.page || 1;
+    const limit = 3;
+    const offset = (page - 1) * limit;
+
+    let sql = "select count(*) as total from todo";
+    if (params.length > 0) {
+      sql += ` where ${params.join(" and ")}`;
+    }
+
+    db.query(sql, (err, raw) => {
+      const jumlahHalaman = Math.ceil(Number(raw.total) / limit);
+      sql = `select * from todo`;
+      if (params.length > 0) {
+        sql += ` where ${params.join(" and ")}`;
+      }
+      sql += ` limit $1 offset $2`;
+
+      console.log(sql);
+
+      db.query(sql, [limit, offset], (err, raws) => {
+        console.log(raws.rows);
+        if (err) return res.send(err);
+        res.render("list", {
+          data: raws.rows,
+          page,
+          jumlahHalaman,
+          query: req.query,
+          url,
+        });
+      });
+    });
+  });
+
+  return router;
+};
